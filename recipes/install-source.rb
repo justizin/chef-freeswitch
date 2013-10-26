7279787c0022994a61502309bf86462428d4947f
Justin Alan Ryan <Justin_Ryan@gap.com>
2013-10-26 12:49:17 -0700

    skeletal testing code for source build of freeswitch.

--- recipes/install-source.rb
+++ recipes/install-source.rb
@@ -1,3 +1,49 @@
-include_recipe "build-essential"
+#
+# Cookbook Name:: freeswitch
+# Recipe:: install-source
+#
+# Author:: Justin Alan Ryan (<bitmonk@bitmonk.net>)
+#
+# Copyright 2013, Justin Alan Ryan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 
+# Roughly following http://wiki.freeswitch.org/wiki/Linux_Quick_Install_Guide
+
+include_recipe 'build-essential'
+
+# totally debuntufied and flat right now
+
+package 'libjpeg-dev'
+package 'libncurses-dev'
+
+# deps for optional features
+
+%w{ libcurl4-openssl-dev libexpat1-dev libssl-dev libtiff4-dev libx11-dev unixodbc-dev \
+    python2.6-dev zlib1g-dev libzrtpcpp-dev libasound2-dev libogg-dev libvorbis-dev libperl-dev \
+    libgdbm-dev libdb-dev python-dev uuid-dev bison autoconf g++ libncurses-dev }.each do |pkgname|
+  package pkgname
+end
+
+git "#{Chef::Config[:file_cache_path]}/app_name" do
+  repository node['freeswitch']['git_repository']
+  reference  node['freeswitch']['git_revision']
+  action :sync
+  notifies :run, 'bash[compile_freeswitch]'
+end
+
+bash 'compile_freeswitch' do
+  command 'sh ./bootstrap.sh && ./configure --prefix=/opt/freeswitch/ && make && make all install cd-sounds-install cd-moh-install'
+end
