fd0ff9ae46613430a9b7f67d2c4982b289b096ec
Justin Alan Ryan <Justin_Ryan@gap.com>
2013-10-26 13:21:22 -0700

    bump version.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'bitmonk@bitmonk.net'
 license          'Apache'
 description      'Installs/Configures freeswitch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.6'
+version          '0.1.7'
 
 depends "build-essential"
